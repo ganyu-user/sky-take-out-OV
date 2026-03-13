b68307750dc7b9dacf10d369a579954d093680f5
ganyu <3050348752@qq.com>
2026-03-14 00:15:18 +0800

    C端-购物车查看、清空、删除一个商品功能业务代码开发

--- sky-server/src/main/java/com/sky/service/impl/ShoppingCartServiceImpl.java
+++ sky-server/src/main/java/com/sky/service/impl/ShoppingCartServiceImpl.java
@@ -71,4 +71,54 @@ public class ShoppingCartServiceImpl implements ShoppingCartService {
             shoppingCartMapper.insert(shoppingCart);
         }
     }
+
+    /**
+     * 查看购物车
+     * @return
+     */
+    public List<ShoppingCart> showShoppingCart() {
+        Long userId = BaseContext.getCurrentId();
+        ShoppingCart shoppingCart = new ShoppingCart();
+        shoppingCart.setUserId(userId);
+        List<ShoppingCart> list = shoppingCartMapper.list(shoppingCart);
+        return list;
+    }
+
+    /**
+     * 清空购物车
+     * @return
+     */
+    public void cleanShoppingCart() {
+        Long userId = BaseContext.getCurrentId();
+        shoppingCartMapper.cleanByUserId(userId);
+    }
+
+    /**
+     * 删除购物车中的一个商品
+     * @param shoppingCartDTO
+     * @return
+     */
+    public void subShoppingCart(ShoppingCartDTO shoppingCartDTO) {
+        //获取购物车中要删除的商品数据
+        ShoppingCart shoppingCart = new ShoppingCart();
+        BeanUtils.copyProperties(shoppingCartDTO, shoppingCart);
+        Long userId = BaseContext.getCurrentId();
+        shoppingCart.setUserId(userId);
+        List<ShoppingCart> list = shoppingCartMapper.list(shoppingCart);
+
+        if (list != null && list.size() > 0) {
+            shoppingCart= list.get(0);
+            Integer number = shoppingCart.getNumber();
+
+            //如果=1，删除这条商品
+            if(number==1){
+                shoppingCartMapper.deleteByNumber(shoppingCart.getId());
+            }else{
+                //如果>=2，数量减一
+                shoppingCart.setNumber(number-1);
+                shoppingCartMapper.updataNumberById(shoppingCart);
+            }
+        }
+
+    }
 }
