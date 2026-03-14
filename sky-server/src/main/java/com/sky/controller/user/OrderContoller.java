2653604cd0e7510b36d3ddcd840262f56a5ae3c1
ganyu <3050348752@qq.com>
2026-03-15 01:00:07 +0800

    订单支付功能代码开发（绕过微信支付，直接把支付后的订单状态和支付状态返回数据库）

--- sky-server/src/main/java/com/sky/controller/user/OrderContoller.java
+++ sky-server/src/main/java/com/sky/controller/user/OrderContoller.java
@@ -1,17 +1,16 @@
 package com.sky.controller.user;
 
+import com.sky.dto.OrdersPaymentDTO;
 import com.sky.dto.OrdersSubmitDTO;
 import com.sky.result.Result;
 import com.sky.service.OrderService;
+import com.sky.vo.OrderPaymentVO;
 import com.sky.vo.OrderSubmitVO;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RequestMapping("/user/order")
 @RestController("userOrderController")
@@ -33,5 +32,20 @@ public class OrderContoller {
         log.info("用户下单："+ordersSubmitDTO);
         OrderSubmitVO orderSubmitVO=orderService.submitOrder(ordersSubmitDTO);
         return Result.success(orderSubmitVO);
+    }
+
+    /**
+     * 订单支付
+     * @param orderPaymentDTO
+     * @return
+     * @throws Exception
+     */
+    @PutMapping("/payment")
+    @ApiOperation("订单支付")
+    public Result<OrderPaymentVO> payment(@RequestBody OrdersPaymentDTO orderPaymentDTO) throws Exception{
+        log.info("订单支付："+orderPaymentDTO);
+        OrderPaymentVO orderPaymentVO=orderService.payment(orderPaymentDTO);
+        log.info("生成预支付教育单："+orderPaymentVO);
+        return Result.success(orderPaymentVO);
     }
 }
